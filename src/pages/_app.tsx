7818bee5ea7850fda830c04fbc34ae1d54919052
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-05-04 17:18:28 +0330

    feat: add redux and toast wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { store } from "@app/store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <ToastContainer rtl={false} pauseOnFocusLoss />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
