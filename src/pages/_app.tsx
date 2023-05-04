51cf2280ce2b28735ba51699be120c65ac414a4c
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-05-04 19:41:03 +0330

    feat: add layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import { ToastContainer } from "react-toastify";
-import { store } from "@app/store";
+import '@styles/globals.scss';
+import {ReactElement} from 'react';
+import {NextPage} from 'next';
+import type {AppProps} from 'next/app';
+import {Provider} from 'react-redux';
+import {ToastContainer} from 'react-toastify';
+import {store} from '@app/store';
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactElement<any, any> | null
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout): React.ReactElement<any, any> | null {
+  const getLayout = Component.getLayout ?? function(page) {
+    return page;
+  };
   return (
     <Provider store={store}>
       <ToastContainer rtl={false} pauseOnFocusLoss />
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </Provider>
   );
 }
